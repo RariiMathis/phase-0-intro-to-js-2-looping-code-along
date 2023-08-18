52a79a622831e2b553289c7aa6a4d3446d39b297
Rarii Mathis <juniormathis82@gmail.com>
2023-08-18 19:52:13 -0400

    not tested

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function beforeEach(){
+    spy = sinon.spy(console,`log`);
+}
+function afterEach(){
+    spy.restore();
+}
+function writeCards([]){
+    for (let spy = 0; spy < array.length; spy++) {
+        const element = array[spy];
+        
+    }return (writeCards(`Thank you,`[writeCards]`,for the wonderful surprise gift!`))   
+}
+function afterEach(countDown){
+    afterEach(countDown(10)).console.log(spy.callCount);;
+    countDown.spy.calledWithExactly(4)
+}
